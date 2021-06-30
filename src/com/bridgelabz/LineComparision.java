5096cfd5afe1bafac8eb00887c80e2e9d591b994
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-30 17:21:17 +0530

    MODIFIED: to chcek two lines equal or not

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -6,19 +6,36 @@ public class LineComparision
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("welcome to line comparision copmutation");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the co-ordinate x1:");
-		double x1=sc.nextDouble();
-		System.out.println("enter the co-ordinate y1:");
-		double y1=sc.nextDouble();
-		System.out.println("enter the co-ordinate x2:");
-		double x2=sc.nextDouble();
-		System.out.println("enter the co-ordinate y2:");
-		double y2=sc.nextDouble();
-		
-		double Lenght = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		System.out.println("Lenth of line is:"+Lenght);
-	}
+		System.out.println("Welcome to the Line Comparision Program");
+		Scanner s = new Scanner(System.in);
+        	System.out.println("Enter x co-ordinate of first point x1: ");
+       		 int x1 = s.nextInt();
+        	System.out.println("Enter y co-ordinate of first point y1: ");
+        	int y1 = s.nextInt();
+        	System.out.println("Enter x co-ordinate of second point x2: ");
+        	int x2 = s.nextInt();
+        	System.out.println("Enter y co-ordinate of second point y2: ");
+        	int y2 = s.nextInt();
 
+		System.out.println("Enter x co-ordinate of third point: ");
+		int p1 = s.nextInt();
+		System.out.println("Enter y co-ordinate of third point: ");
+		int q1 = s.nextInt();
+		System.out.println("Enter x co-ordinate of forth point: ");
+		int p2 = s.nextInt();
+		System.out.println("Enter y co-ordinate of forth point: ");
+		int q2 = s.nextInt();
+
+       	 	Double length_of_line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        	Double length_of_line2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+
+		System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
+		System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);
+		boolean ans = length_of_line1.equals(length_of_line2);
+		if(ans)
+				System.out.println("Both lines are equal.");
+		else
+				System.out.println("Both lines are not equal.");	
+	}
 }
+
